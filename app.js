bf75b3b7aff3d7de359cd3e4b04d2b764771c9bd
Nakul Prasad <nakulprasad10@outlook.com>
2024-05-18 13:51:44 +0530

    Added Table Show

--- app.js
+++ app.js
@@ -1,20 +1,22 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 
 app.get('/', (req, res) => {
     res.send('Hello, World!');
 });
 
+app.use(cors());
 
-// Serve static files from the React app in production
-if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(path.resolve(), './client/build')));
-    app.get('*', function (req, res) {
-        res.sendFile(path.join(path.resolve(), "./client/build/index.html"), function (err) {
-            res.status(500).send(err);
-        })
-    });
-}
+// // Serve static files from the React app in production
+// if (process.env.NODE_ENV === "production") {
+//     app.use(express.static(path.join(path.resolve(), './client/build')));
+//     app.get('*', function (req, res) {
+//         res.sendFile(path.join(path.resolve(), "./client/build/index.html"), function (err) {
+//             res.status(500).send(err);
+//         })
+//     });
+// }
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
---
+++ frontend/src/lib/URLS.js
@@ -0,0 +1,5 @@
+const BASEURL= 'http://localhost:3000/api'
+export const URLS = {
+    POSTEXCEL: `${BASEURL}/upload`,
+    POSTMAIL: `${BASEURL}/sendmail`
+};
